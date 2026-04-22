43246b71b6fe72e6f848d6098cb5d6f73f8c693b
automatech09 <automatech09000@gmail.com>
2026-04-22 07:25:09 +0200

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './types/templates';
-export * from './lib/supabase/client';
--- dist/lib/supabase/client.d.ts
+++
@@ -1,3 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-export declare function createSupabaseBrowserClient(url: string, key: string): import("@supabase/supabase-js").SupabaseClient<any, "public", "public", any, any>;
-export declare function getCurrentUserId(supabase: ReturnType<typeof createBrowserClient>): Promise<string>;
